59c5ade8cb442f2f4a8b4e3cbd642b7b25b8efb1
Thomas <ThomasJamesFowler97@gmail.com>
2017-09-06 11:14:23 +1000

    Added Getters and Setters.

--- src/main/java/com/tfowl/project/world/World.java
+++ src/main/java/com/tfowl/project/world/World.java
@@ -4,6 +4,7 @@ import com.tfowl.project.entity.Entity;
 import com.tfowl.project.entity.Player;
 import com.tfowl.project.level.Level;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,4 +15,32 @@ public class World {
 	private Level level;
 	private List<Entity> entities;
 	private Player player;
+
+	public World() {
+		entities = new ArrayList<>();
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+
+	public List<Entity> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(List<Entity> entities) {
+		this.entities = entities;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
 }
