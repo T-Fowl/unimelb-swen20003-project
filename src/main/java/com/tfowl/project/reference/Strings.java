7e148e238890ea82eed0d0453c599f9058bb3ae7
Thomas <ThomasJamesFowler97@gmail.com>
2017-09-07 19:43:18 +1000

    Finished project part 1.

--- src/main/java/com/tfowl/project/reference/Strings.java
+++ src/main/java/com/tfowl/project/reference/Strings.java
@@ -8,14 +8,15 @@ package com.tfowl.project.reference;
 public class Strings {
 
 
-	/* Field should be filled-in by gradle when building and exporting the project */
+	/* Field should be filled-in by gradle when exporting the project */
 	public static final String STUDENT_ID = "@STUDENTID@";
+	public static final String STUDENT_NAME = "@STUDENTNAME@";
 
 	/* Fifty percent pain
 	 * And a hundred percent reason to remember the name */
 	public static final String GAME_NAME = "Shadow Blocks";
 
-	public static final String DEFAULT_WINDOW_TITLE = GAME_NAME + " (Student id: " + STUDENT_ID + ")";
+	public static final String DEFAULT_WINDOW_TITLE = GAME_NAME + " by " + STUDENT_NAME + "( " + STUDENT_ID + ")";
 
 	/* No instancing */
 	private Strings() {
