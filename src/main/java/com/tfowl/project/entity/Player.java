fc6e6014b93a44766351e47f908158d3283d6a01
Thomas <ThomasJamesFowler97@gmail.com>
2017-09-07 11:07:43 +1000

    Added some location attributes to Player

--- src/main/java/com/tfowl/project/entity/Player.java
+++ src/main/java/com/tfowl/project/entity/Player.java
@@ -6,4 +6,32 @@ package com.tfowl.project.entity;
 public class Player implements Entity {
 
 	public static final String PLAYER_TILE_NAME = "player";
+
+	private int xCoordinate;
+	private int yCoordinate;
+
+	public Player() {
+		this(0, 0);
+	}
+
+	public Player(int xCoordinate, int yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+
+	public int getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(int xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public int getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(int yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
 }
