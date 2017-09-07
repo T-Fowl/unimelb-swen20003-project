51872fa87e0054dfa1c8f0b63fc59af2f8d513e1
Thomas <ThomasJamesFowler97@gmail.com>
2017-09-07 12:30:28 +1000

    Added documentation for IRenderable

--- src/main/java/com/tfowl/project/graphics/IRenderable.java
+++ src/main/java/com/tfowl/project/graphics/IRenderable.java
@@ -5,20 +5,52 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 /**
+ * Represents something that can be rendered. The primary purpose of this class is to handle
+ * drawing something that is centered without said component or its parent having to worry
+ * about the necessary offsets.
+ *
  * Created by Thomas on 7/09/2017.
  */
 public interface IRenderable {
 
+	/**
+	 * Get or compute the rendered width of this {@link IRenderable}.
+	 * Defaults to {@link Graphical#TILE_SIDE_LENGTH}.
+	 * @return The rendered width of this {@link IRenderable}
+	 * @throws SlickException If the underlying slick library throws an exception.
+	 */
 	public default int getRenderedWidth() throws SlickException {
 		return Graphical.TILE_SIDE_LENGTH;
 	}
 
+	/**
+	 * Get or compute the rendered height of this {@link IRenderable}.
+	 * Defaults to {@link Graphical#TILE_SIDE_LENGTH}.
+	 * @return The rendered height of this {@link IRenderable}
+	 * @throws SlickException If the underlying slick library throws an exception.
+	 */
 	public default int getRenderedHeight() throws SlickException {
 		return Graphical.TILE_SIDE_LENGTH;
 	}
 
+	/**
+	 * Draw this {@link IRenderable} with its origin at (gx, gy).
+	 * @param g Graphics context.
+	 * @param gx Origin x.
+	 * @param gy Origin y.
+	 * @throws SlickException If the underlying slick library throws an exception.
+	 */
 	public void draw(Graphics g, int gx, int gy) throws SlickException;
 
+	/**
+	 * Draw this {@link IRenderable} with its center at (gx, gy). Default implementation is to
+	 * draw with the origin translated away from the origin by half the width and height in
+	 * their respective axis.
+	 * @param g Graphics context.
+	 * @param gx Center x.
+	 * @param gy Center y.
+	 * @throws SlickException If the underlying slick library throws an exception.
+	 */
 	public default void drawCentered(Graphics g, int gx, int gy) throws SlickException {
 		draw(g, gx - getRenderedWidth() / 2, gy - getRenderedHeight() / 2);
 	}
