ef4c5b34db079c11b7e62ea6c633e5b9d30c80a5
Thomas <ThomasJamesFowler97@gmail.com>
2017-09-07 10:39:32 +1000

    Added a way to load (& cache) Tiles.

--- src/main/java/com/tfowl/project/tile/Tile.java
+++ src/main/java/com/tfowl/project/tile/Tile.java
@@ -1,13 +1,42 @@
 package com.tfowl.project.tile;
 
 
+import com.tfowl.project.util.ResourceLoader;
 import org.newdawn.slick.Image;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by Thomas on 6/09/2017.
  */
 public class Tile {
 
+	private static ConcurrentMap<String, Tile> loadedTiles;
+
+	static {
+		loadedTiles = new ConcurrentHashMap<>();
+	}
+
 	private String name;
 	private Image sprite;
+
+	public Tile(String name, Image sprite) {
+		this.name = name;
+		this.sprite = sprite;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static Optional<Tile> getTileFromName(String name) {
+		return Optional.ofNullable(
+				loadedTiles.computeIfAbsent(name.toLowerCase().trim(), tileName -> {
+					Image tileImage = ResourceLoader.getImageResource(tileName);
+					return null == tileImage ? null : new Tile(tileName, tileImage);
+				})
+		);
+	}
 }
--- src/main/java/com/tfowl/project/util/ResourceLoader.java
+++ src/main/java/com/tfowl/project/util/ResourceLoader.java
@@ -1,5 +1,8 @@
 package com.tfowl.project.util;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 import java.io.InputStream;
 import java.net.URL;
 
@@ -7,6 +10,21 @@ import java.net.URL;
  * Created by Thomas on 5/09/2017.
  */
 public class ResourceLoader {
+
+	private static final String IMAGE_RESOURCE_DIR = "/images";
+	private static final String DEFAULT_IMAGE_EXRENSION = ".png";
+	private static final String LEVEL_RESOURCE_DIR = "/levels";
+
+	public static Image getImageResource(String ref) {
+		if (ref.indexOf('.') < 0) {
+			ref = ref + DEFAULT_IMAGE_EXRENSION;
+		}
+		try {
+			return new Image(getResourceAsStream(IMAGE_RESOURCE_DIR + "/" + ref), ref, false);
+		} catch (SlickException e) {
+			return null;
+		}
+	}
 
 	public static URL getResource(String ref) {
 		return org.newdawn.slick.util.ResourceLoader.getResource(ref);
