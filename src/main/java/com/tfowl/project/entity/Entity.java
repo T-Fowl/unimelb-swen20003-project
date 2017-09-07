9d0759c7f817e20a7a6c40a89f6cdf22a17954e1
Thomas <ThomasJamesFowler97@gmail.com>
2017-09-07 12:34:56 +1000

    Moved some attributes and methods from Player into Entity.

--- src/main/java/com/tfowl/project/entity/Entity.java
+++ src/main/java/com/tfowl/project/entity/Entity.java
@@ -3,5 +3,48 @@ package com.tfowl.project.entity;
 /**
  * Created by Thomas on 6/09/2017.
  */
-public interface Entity {
+public class Entity {
+
+	private String name;
+
+	private int x;
+	private int y;
+
+	public Entity(String name) {
+		this(name, 0, 0);
+	}
+
+	public Entity(String name, int x, int y) {
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int moveX(int delta) {
+		int newX = getX() + delta;
+		setX(newX);
+		return newX;
+	}
+
+	public int moveY(int delta) {
+		int newY = getY() + delta;
+		setY(newY);
+		return newY;
+	}
 }
--- src/main/java/com/tfowl/project/entity/Player.java
+++ src/main/java/com/tfowl/project/entity/Player.java
@@ -9,49 +9,18 @@ import org.newdawn.slick.SlickException;
 /**
  * Created by Thomas on 6/09/2017.
  */
-public class Player implements Entity, IRenderable {
+public class Player extends Entity implements IRenderable {
 
 	public static final String PLAYER_TILE_NAME = "player";
 
-	private int xCoordinate;
-	private int yCoordinate;
 	private Image sprite;
 
 	public Player() {
-		this(0, 0);
-	}
-
-	public Player(int xCoordinate, int yCoordinate) {
-		this.xCoordinate = xCoordinate;
-		this.yCoordinate = yCoordinate;
+		super(PLAYER_TILE_NAME);
 	}
 
 	public void init() throws SlickException {
 		sprite = ResourceLoader.getImageResource(PLAYER_TILE_NAME);
-	}
-
-	public int getX() {
-		return xCoordinate;
-	}
-
-	public void setX(int xCoordinate) {
-		this.xCoordinate = xCoordinate;
-	}
-
-	public void moveX(int delta) {
-		setX(getX() + delta);
-	}
-
-	public int getY() {
-		return yCoordinate;
-	}
-
-	public void setY(int yCoordinate) {
-		this.yCoordinate = yCoordinate;
-	}
-
-	public void moveY(int delta) {
-		setY(getY() + delta);
 	}
 
 	@Override
