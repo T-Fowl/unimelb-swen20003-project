221bac39f42bce5fd9eaea68bdfd111f7917be91
Thomas <ThomasJamesFowler97@gmail.com>
2017-10-13 01:05:25 +1100

    Added a simple world state tracking data class.

---
+++ src/main/java/com/tfowl/project/world/WorldState.java
@@ -0,0 +1,35 @@
+package com.tfowl.project.world;
+
+import com.tfowl.project.block.IBlockState;
+import com.tfowl.project.impl.BlockInstance;
+import com.tfowl.project.util.Position;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorldState {
+
+	private Position playerPosition;
+	private Map<BlockInstance, Map.Entry<Position, IBlockState>> blockStates;
+
+	public WorldState() {
+		blockStates = new HashMap<>();
+	}
+
+	public Position getPlayerPosition() {
+		return playerPosition;
+	}
+
+	public void setPlayerPosition(Position playerPosition) {
+		this.playerPosition = playerPosition;
+	}
+
+	public Map<BlockInstance, Map.Entry<Position, IBlockState>> getBlockStates() {
+		return blockStates;
+	}
+
+	public void withBlockState(BlockInstance instance, Position position, IBlockState state) {
+		blockStates.put(instance, new AbstractMap.SimpleEntry<>(position, state));
+	}
+}
