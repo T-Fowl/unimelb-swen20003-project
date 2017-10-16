cb14c3e13ba00c3a1f2d51a8060b2ddd77335371
Thomas <ThomasJamesFowler97@gmail.com>
2017-10-16 21:22:40 +1100

    Improved the menu layout

--- src/main/java/com/tfowl/shadowblocks/game/GameStateMenu.java
+++ src/main/java/com/tfowl/shadowblocks/game/GameStateMenu.java
@@ -1,15 +1,23 @@
 package com.tfowl.shadowblocks.game;
 
 import com.tfowl.shadowblocks.reference.Resources;
+import com.tfowl.shadowblocks.util.ResourceLoader;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
 import org.newdawn.slick.*;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.ComponentListener;
 import org.newdawn.slick.gui.MouseOverArea;
-import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
+import org.newdawn.slick.util.FontUtils;
+
+import java.awt.*;
+import java.io.IOException;
 
 
 public class GameStateMenu extends BasicGameState implements ComponentListener {
@@ -28,24 +36,57 @@ public class GameStateMenu extends BasicGameState implements ComponentListener {
 	private MouseOverArea btnHighScores;
 	private MouseOverArea btnQuit;
 
+	private Font font;
+	private float fontSize = 30;
+
+	private Color btnNormalColour = Color.blue;
+	private Color btnMouseOverColour = Color.cyan;
+	private Color btnMouseDownColour = Color.orange;
+
 
 	@Override
 	public void init(GameContainer container, StateBasedGame stateBasedGame) throws SlickException {
 		this.game = stateBasedGame;
 
-		btnSinglePlayer = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnSinglePlayer.png"), 100, 100, this);
-		btnHighScores = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnHighscores.png"), 100, 200, this);
-		btnQuit = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnQuit.png"), 100, 300, this);
-		btnMultiplayer = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnMultiplayer.png"), 100, 400, this);
+		try {
+			java.awt.Font awtFont = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, ResourceLoader.getResourceAsStream("fonts/opensans/OpenSans-Regular.ttf"))
+					.deriveFont(fontSize);
+			font = new TrueTypeFont(awtFont, true);
+		} catch (FontFormatException | IOException e) {
+			e.printStackTrace();
+			font = new TrueTypeFont(java.awt.Font.getFont(java.awt.Font.SANS_SERIF)
+					.deriveFont(fontSize), true);
+		}
+
+		btnSinglePlayer = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnSinglePlayer.png"),
+				container.getWidth() / 2 - 200 / 2, 150, this);
+		btnSinglePlayer.setNormalColor(btnNormalColour);
+		btnSinglePlayer.setMouseOverColor(btnMouseOverColour);
+		btnSinglePlayer.setMouseDownColor(btnMouseDownColour);
+		btnHighScores = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnHighscores.png"),
+				container.getWidth() / 2 - 200 / 2, 250, this);
+		btnHighScores.setNormalColor(btnNormalColour);
+		btnHighScores.setMouseOverColor(btnMouseOverColour);
+		btnHighScores.setMouseDownColor(btnMouseDownColour);
+		btnQuit = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnQuit.png"),
+				container.getWidth() / 2 - 200 / 2, 350, this);
+		btnQuit.setNormalColor(btnNormalColour);
+		btnQuit.setMouseOverColor(btnMouseOverColour);
+		btnQuit.setMouseDownColor(btnMouseDownColour);
+		btnMultiplayer = new MouseOverArea(container, new Image(Resources.IMAGES_DIRECTORY + "/btnMultiplayer.png"),
+				container.getWidth() / 2 - 200 / 2, 450, this);
+		btnMultiplayer.setNormalColor(btnNormalColour);
+		btnMultiplayer.setMouseOverColor(btnMouseOverColour);
+		btnMultiplayer.setMouseDownColor(btnMouseDownColour);
 	}
 
 	@Override
-	public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-		graphics.drawString("Choose an option", 100, 50);
-		btnSinglePlayer.render(gameContainer, graphics);
-		btnHighScores.render(gameContainer, graphics);
-		btnQuit.render(gameContainer, graphics);
-		btnMultiplayer.render(gameContainer, graphics);
+	public void render(GameContainer container, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+		FontUtils.drawCenter(font, "Choose An Option", 0, 50, container.getWidth());
+		btnSinglePlayer.render(container, graphics);
+		btnHighScores.render(container, graphics);
+		btnQuit.render(container, graphics);
+		btnMultiplayer.render(container, graphics);
 	}
 
 	@Override
